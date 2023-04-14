747f3063f52957f308aaa388224278f5cf928445
윤수현 <ysh820601@naver.com>
2023-04-14 16:12:37 +0900

    setting...

--- boot.sh
+++ boot.sh
@@ -1,1 +1,1 @@
-gunicorn --bind 0:8000 main:app --worker-class uvicorn.workers.UvicornWorker
+gunicorn --bind 0:8000 main:app --worker-class uvicorn.workers.UvicornWorker --log-config /Users/yunsuhyeon/fastapi/venvs/myapi/logs/uvicorn_log.ini
