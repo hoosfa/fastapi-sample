aa345df02e55783067a765d885c457818db7d43b
YSH <ysh820601@naver.com>
2023-04-14 14:10:26 +0900

    fastapi init

---
+++ frontend/src/App.svelte
@@ -0,0 +1,62 @@
+<script>
+  import Router from 'svelte-spa-router'
+  import Home from './routes/Home.svelte'
+  import Detail from './routes/Detail.svelte'
+  import QuestionCreate from "./routes/QuestionCreate.svelte"
+  import Navigation from "./components/Navigation.svelte"
+  import UserCreate from "./routes/UserCreate.svelte"
+  import UserLogin from "./routes/UserLogin.svelte"
+  import QuestionModify from "./routes/QuestionModify.svelte"
+  import AnswerModify from "./routes/AnswerModify.svelte"
+
+  const routes = {
+    '/':Home,
+    '/detail/:question_id':Detail,
+    '/question-create': QuestionCreate,
+    '/user-create': UserCreate,
+    '/user-login': UserLogin,
+    '/question-modify/:question_id': QuestionModify,
+    '/answer-modify/:answer_id': AnswerModify,
+  }
+
+</script>
+
+
+<Navigation />
+<Router {routes}></Router>
+
+<!-- fetch 사용법 01 -->
+<!-- <script>
+  let message;
+
+  fetch("http://127.0.0.1:8000/hello").then((response) => {
+    response.json().then((json) => {
+      message = json.message;
+    });
+  });
+</script>
+
+<h1>{message}</h1> -->
+
+
+<!-- fetch 사용법 02 -->
+<!-- <script>
+  async function hello() {
+    const res = await fetch("http://127.0.0.1:8000/hello");
+    const json = await res.json();
+
+    if (res.ok) {
+      return json.message;
+    } else {
+      alert("error");
+    }
+  }
+
+  let promise = hello();
+</script>
+
+{#await promise}
+  <p>...waiting</p>
+{:then message}
+  <h1>{message}</h1>
+{/await} -->
